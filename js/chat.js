a94287db18a4bcffdec89a9aa1d99756efed118f
bahuma20 <bahuma20@bahuma-online.de>
2014-03-12 18:22:34 +0100

    debugging

--- js/chat.js
+++ js/chat.js
@@ -2,7 +2,9 @@ var latestid = 0;
 
 function getLatestEntries() {
 	$messages = jQuery.getJSON("chatcontroller.php?action=getLatestEntries&latestID="+latestid+"&room=default", function (data) {
-		jQuery.each(data, function () {
+		jQuery.each(data, function (i, el) {
+			console.log(i);
+			console.log(el);
 			jQuery("#chatwindow").append('<div class="message"><div class="author">'+ jQuery(this).user + '</div><div class="content">'+jQuery(this).content+'</div></div>');
 		});
 	});
