96f87ba8e49f693ec3db4eeba5369adb5b98a287
bahuma20 <bahuma20@bahuma-online.de>
2014-03-12 18:32:13 +0100

    Interval fix

--- js/chat.js
+++ js/chat.js
@@ -4,14 +4,12 @@ function getLatestEntries() {
 	$messages = jQuery.getJSON("chatcontroller.php?action=getLatestEntries&latestID="+latestid+"&room=default", function (data) {
 		jQuery.each(data, function (i, el) {
 			jQuery("#chatwindow").append('<div class="message"><div class="time">'+ el.time +'</div><div class="author">'+ el.user + '</div><div class="content">'+ el.content+'</div></div>');
+			
+			
 		});
 	});
 }
 
-function handleLatestEntries () {
-	
-}
-
 jQuery(document).ready(function () {	
-	setInterval(getLatestEntries(), 1000);
+	setInterval("getLatestEntries()", 1000);
 });
