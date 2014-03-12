7a3994266dbcccb90945e97dd57849033d2f9a94
bahuma20 <bahuma20@bahuma-online.de>
2014-03-12 18:16:07 +0100

    Change path to chatcontroller.php

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,7 @@
 var latestid = 0;
 
 function getLatestEntries() {
-	$messages = jQuery.getJSON("../chatcontroller.php?action=getLatestEntries&latestID="+latestid+"&room=default", function (data) {
+	$messages = jQuery.getJSON("chatcontroller.php?action=getLatestEntries&latestID="+latestid+"&room=default", function (data) {
 		data.each(function () {
 			jQuery("#chatwindow").append('<div class="message"><div class="author">'+ jQuery(this).user + '</div><div class="content">'+jQuery(this).content+'</div></div>');
 		});
