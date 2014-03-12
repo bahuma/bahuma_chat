ffb4920a087c59497a3d4cd04bf2efe50eddd851
bahuma20 <bahuma20@bahuma-online.de>
2014-03-12 18:24:26 +0100

    Add time display

--- js/chat.js
+++ js/chat.js
@@ -3,9 +3,7 @@ var latestid = 0;
 function getLatestEntries() {
 	$messages = jQuery.getJSON("chatcontroller.php?action=getLatestEntries&latestID="+latestid+"&room=default", function (data) {
 		jQuery.each(data, function (i, el) {
-			console.log(i);
-			console.log(el);
-			jQuery("#chatwindow").append('<div class="message"><div class="author">'+ jQuery(this).user + '</div><div class="content">'+jQuery(this).content+'</div></div>');
+			jQuery("#chatwindow").append('<div class="message"><div class="time">'+ el.time +'</div><div class="author">'+ el.user + '</div><div class="content">'+ el.content+'</div></div>');
 		});
 	});
 }
