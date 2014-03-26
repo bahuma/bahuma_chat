58f7328a6b95b1dd3c9321564748f074e6ade082
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-03-26 09:45:03 -0400

    Add Chatcontroller

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var app = express();
 
 // Setup port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT;
 
 // Initialize socket.io
 var io = require('socket.io').listen(app.listen(port));
 
 // Do config and routes
 require('./config')(app, io);
-require('./routes')(app,io);
+require('./routes')(app, io);
+require('./chatcontroller')(app, io);
 
 console.log('Bahuma Chat is running on ' + process.env.IP + ":" + port);
