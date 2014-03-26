8bfdf35ce36d4323e200792b6450e2675061d382
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-03-26 10:14:14 -0400

    Added emit handlers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // Setup port
-var port = process.env.PORT;
+var port = process.env.PORT || 8080;
 
 // Initialize socket.io
 var io = require('socket.io').listen(app.listen(port));
--- chatcontroller.js
+++ chatcontroller.js
@@ -1,3 +1,30 @@
 module.exports = function (app, io) {
-    
+    var chat = io.of('/chat').on('connection', function (socket) {
+       
+       // New Message
+       socket.on('newMessage', function (data) {
+           
+       });
+       
+       // New Message
+       socket.on('newMessage', function (data) {
+           
+       });
+       
+       // Kick
+       socket.on('kick', function (data) {
+           
+       });
+       
+       // Refresh
+       socket.on('refresh', function (data) {
+           
+       });
+       
+       // Get online users
+       socket.on('getOnlineUsers', function (data) {
+           
+       });
+        
+    });
 }
