d3f0974dca2f0bbb9025cdb799fbd1393cc3b3fb
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-03-26 09:23:30 -0400

    Add config and routes

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ var port = process.env.PORT || 8080;
 // Initialize socket.io
 var io = require('socket.io').listen(app.listen(port));
 
+// Do config and routes
+require('./config')(app, io);
+require('./routes')(app,io);
+
 console.log('Bahuma Chat is running on ' + process.env.IP + ":" + port);
