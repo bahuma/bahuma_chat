86efca9c689daca4445124d9f4e660db2cc7b0a1
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-03-26 14:41:44 -0400

    ÂPerparing for openshift

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // Setup port
-var port = process.env.PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 
 // Initialize socket.io
 var io = require('socket.io').listen(app.listen(port));
