b9ff0aaf6fd35a4c7a076f4caeb6dbbba005e7a0
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-03-26 09:26:48 -0400

    Added routes and views

--- routes.js
+++ routes.js
@@ -1,3 +1,14 @@
 module.exports = function (app, io){
-    
+    // Routes
+    app.get('/', function(req, res){
+		res.render('home');
+	});
+	
+	app.get('/login', function(req, res){
+		res.render('login');
+	});
+	
+	app.get('/chat', function(req, res){
+		res.render('chat');
+	});
 };
